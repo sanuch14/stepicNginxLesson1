f49c6ac3d8da3350d84354ffb38cbb49b6458f11
Oleksandr Lisovyi <sansan14@mail.ru>
2016-11-12 13:59:31 +0000

    working code

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 sudo rm /etc/nginx/sites-enabled/test.conf
-#sudo rm /etc/nginx/sited-enabled/default
-sudo ln -s /home/oleksandr/Public/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+
+sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
