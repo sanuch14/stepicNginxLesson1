895f231e5b8f5bfb9617b761bef4104b02d3cd6d
Oleksandr Lisovyi <sansan14@mail.ru>
2016-11-12 14:27:32 +0000

    final version

--- init.sh
+++ init.sh
@@ -1,4 +1,6 @@
 sudo rm /etc/nginx/sites-enabled/test.conf
+sudo rm /etc/nginx/sites-enabled/default
 
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+#sudo ls -s /home/box/web/etc/nginx.co
 sudo /etc/init.d/nginx restart
